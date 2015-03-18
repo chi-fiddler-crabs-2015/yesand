0c3b19a49144f6426d8a5489c5ebf7ef982bfd6d
Ricardo Fleury <rfleury2@gmail.com>
2015-03-18 17:27:46 -0500

    Add idea validation to model

--- yesand/app/models/idea.rb
+++ yesand/app/models/idea.rb
@@ -3,4 +3,12 @@ class Idea < ActiveRecord::Base
 	has_many :comments
 	has_many :votes
 
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+
+  validates_length_of :title, :maximum => 120
+  validates_length_of :title, :minimum => 10
+
+  validates_length_of :description, :minimum => 10
+
 end

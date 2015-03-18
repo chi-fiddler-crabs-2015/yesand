9a38e828b5e57b736891f1b05e64ce10f7272c94
Ricardo Fleury <rfleury2@gmail.com>
2015-03-18 15:52:15 -0500

    Change index location to application controller

--- yesand/app/controllers/application_controller.rb
+++ yesand/app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    redirect_to ideas_path
+  end
+
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- yesand/app/controllers/index_controller.rb
+++
@@ -1,5 +0,0 @@
-class IndexController < ApplicationController
-  def index
-    redirect_to ideas_path
-  end
-end

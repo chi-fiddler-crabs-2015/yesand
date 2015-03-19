932a57847675b850418bb2b2883f3575ba85c843
Kara A Carrell <karaAJC@gmail.com>
2015-03-18 20:06:01 -0500

    updating gemfile with shoulda matchers and creating user model test all passing

--- yesand/spec/models/user_spec.rb
+++ yesand/spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  it { should have_many(:ideas) }
+describe User do
+  it { should have_many(:ideas).with_foreign_key(:creator_id) }
+
+  it { should have_many(:comments).with_foreign_key(:author_id) }
+
+  it { should have_many(:votes).with_foreign_key(:voter_id) }
+
+  it { should validate_presence_of(:username) }
+
+  it { should validate_presence_of(:email) }
+
+  it { should validate_presence_of(:password_hash) }
+
+  it { should validate_uniqueness_of(:email) }
+
+  it { should validate_uniqueness_of(:username) }
+
 end

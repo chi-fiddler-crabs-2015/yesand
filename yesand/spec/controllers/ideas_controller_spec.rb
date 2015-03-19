ebd2b75b1d14192d643e234ab72041b613cbc3c2
Ricardo Fleury <rfleury2@gmail.com>
2015-03-19 07:42:06 -0500

    Add controller index and new tests

---
+++ yesand/spec/controllers/ideas_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe IdeasController, :type => :controller do
+  describe '#index' do
+    it 'assigns ideas as @ideas' do
+      get :index
+      expect(assigns(:ideas)).to eq(Idea.all)
+    end
+
+    it {
+      get :index
+      should render_template('index')
+    }
+  end
+
+  describe '#new' do
+    it 'assigns a blank unsaved idea as @idea' do
+      get :new
+      expect(assigns(:idea)).to be_a_new Idea
+    end
+
+    it {
+      get :new
+      should render_template('new')
+    }
+  end
+
+end

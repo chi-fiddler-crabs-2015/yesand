448b6b4fa7ac74b7c36fc670b58e676222aabcfc
Kara A Carrell <karaAJC@gmail.com>
2015-03-19 18:15:13 -0500

    updating idea model with creator presence validation, and cleaning up tabs

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,14 +1,16 @@
 class Idea < ActiveRecord::Base
-	belongs_to :creator, class_name: 'User'
-	has_many :comments, as: :commentable
-	has_many :votes, as: :votable
+  belongs_to :creator, class_name: 'User'
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
 
-  validates :title, :description, presence: true
+  validates :title, :description, :creator, presence: true
   validates :title, uniqueness: true
 
   validates_length_of :title, :maximum => 120
   validates_length_of :title, :minimum => 10
 
   validates_length_of :description, :minimum => 10
+
+
 
 end

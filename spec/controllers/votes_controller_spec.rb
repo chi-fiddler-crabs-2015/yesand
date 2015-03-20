13389df3f1213f5145a868a2dca5e92a62bb5fdd
Kara A Carrell <karaAJC@gmail.com>
2015-03-19 20:43:05 -0500

    updating controller specs to pass validation requirements, all tests passing

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe VotesController, :type => :controller do
 
   describe '#create' do
     #### User factory girl to generate these - refactor opportunity
-    let!(:idea) { Idea.create(title: 'hellohello', description: 'hellohellohello') }
-    let!(:user) { User.create(username: 'hello', email: 'hello@hello.com', password: "hello") }
+    let!(:user) { User.create!(username: 'hello', email: 'hello@hello.com', password: "hello") }
+    let!(:idea) { Idea.create!(title: 'hellohello', description: 'hellohellohello', creator: user) }
     before { allow(controller).to receive(:current_user) { user } }
     let!(:comment) { idea.comments.create(author: user, text: 'hellohellohello') }
 

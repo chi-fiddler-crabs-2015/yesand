248ecd693877d2f5a9f9e2d915ce691c3a7baebc
Kara A Carrell <karaAJC@gmail.com>
2015-03-24 20:23:04 -0500

    adding panel for homepage with styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   include TweetButton
 
-  TweetButton.default_tweet_button_options = {:via => "yesand"}
+  TweetButton.default_tweet_button_options = {:via => "TeamYesAnd"}
 
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
